f628180a667126779ece2cfd89d096810294d26f
Chinthaka Jayasooriya <saduchinthaka114@gmail.com>
2021-06-30 16:43:15 +0530

    add log files

--- src/main/java/com/example/logs/controller/CustomerController.java
+++ src/main/java/com/example/logs/controller/CustomerController.java
@@ -2,6 +2,8 @@ package com.example.logs.controller;
 
 import com.example.logs.dto.CustomerDTO;
 import com.example.logs.service.CustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,11 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("customer")
 public class CustomerController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger("customer");
+
     @Autowired
     private CustomerService customerService;
 
     @PostMapping
     public void saveCustomer(@RequestBody CustomerDTO customerDTO){
+        LOGGER.info("request  - POST | customer | saveCustomer | payload : {}",customerDTO);
         customerService.saveCustomer(customerDTO);
+        LOGGER.info("response - POST | customer | saveCustomer | success");
     }
 }
--- src/main/java/com/example/logs/controller/ItemController.java
+++ src/main/java/com/example/logs/controller/ItemController.java
@@ -2,6 +2,8 @@ package com.example.logs.controller;
 
 import com.example.logs.dto.ItemDTO;
 import com.example.logs.service.ItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,14 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("item")
 public class ItemController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("item");
 
     @Autowired
     private ItemService itemService;
 
     @PostMapping
     public void saveItem(@RequestBody ItemDTO itemDTO){
+        LOGGER.info("request  - POST | customer | saveItem | payload : {}",itemDTO);
         itemService.saveItem(itemDTO);
+        LOGGER.info("response - POST | customer | saveItem | success");
     }
 }
